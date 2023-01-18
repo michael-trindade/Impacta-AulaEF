e6fe866e306413f99d54e5719f6ca155191853bf
unknown <email@example.com>
2023-01-17 22:17:37 -0300

    v.0.0.1 - crud completo

---
+++ ControleEscolar/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ControleEscolar.Models;
+
+namespace ControleEscolar.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ControleEscolar.Models.Student> Student { get; set; }
+        public DbSet<ControleEscolar.Models.Grade> Grade { get; set; }
+    }
+}
---
+++ ControleEscolar/Models/Grade.cs
@@ -0,0 +1,11 @@
+﻿namespace ControleEscolar.Models
+{
+    public class Grade
+    {
+        public int GradeId { get; set; }
+        public string GradeName { get; set; }
+        public string Section { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
